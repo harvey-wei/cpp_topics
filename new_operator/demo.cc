08a91d7be8ef104f9262be4b84ba0d5249ee1aeb
Wei MAO <wei.mao@uisee.com>
2022-05-20 15:22:20 +0800

    what does the new operator return?

---
+++ new_operator/demo.cc
@@ -0,0 +1,45 @@
+// operator new example
+#include <iostream>     // std::cout
+#include <new>          // ::operator new
+
+struct MyClass {
+  int data[100];
+  MyClass() {std::cout << "constructed [" << this << "]\n";}
+};
+
+int main ()
+{
+    std::cout << "1: ";
+    MyClass * p1 = new MyClass;
+      // allocates memory by calling: operator new (sizeof(MyClass))
+      // and then constructs an object at the newly allocated space
+
+    std::cout << "2: ";
+    MyClass * p2 = new (std::nothrow) MyClass;
+      // allocates memory by calling: operator new (sizeof(MyClass),std::nothrow)
+      // and then constructs an object at the newly allocated space
+
+    std::cout << "3: ";
+    new (p2) MyClass;
+      // does not allocate memory -- calls: operator new (sizeof(MyClass),p2)
+      // but constructs an object at p2
+
+    // Notice though that calling this function directly does not construct an object:
+    std::cout << "4: ";
+    MyClass * p3 = (MyClass*) ::operator new (sizeof(MyClass));
+      // allocates memory by calling: operator new (sizeof(MyClass))
+      // but does not call MyClass's constructor
+
+    delete p1;
+    delete p2;
+    delete p3;
+
+    return 0;
+}
+
+// Q: What does the new operator return?
+// A: a pointer to the newly allocated storage space in the heap.
+// In T* ptr = new T, the ptr is of type T*.
+// https://stackoverflow.com/questions/2697892/what-is-return-type-of-new-in-c
+// This is why std::shared_ptr is called shared pointer to contain the return values of new
+// operator.
