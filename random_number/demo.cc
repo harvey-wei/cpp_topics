0779ef83adba7c0f2d6bb8c975b50626a3efe691
Wei MAO <wei.mao@uisee.com>
2022-09-29 17:14:21 +0800

    update

---
+++ random_number/demo.cc
@@ -0,0 +1,60 @@
+#include <random>
+#include <iostream>
+
+int main() {
+    /* step 1: set up an engine */
+    std::random_device rd;
+    std::mt19937 rng(rd()); // mt stands for Mersenne twister.
+
+    /* step 2: specify the distribution type. */
+    std::uniform_int_distribution<> uniform_int_dist(1, 6);  // inclusive
+    std::uniform_real_distribution<> uniform_real_dist(10, 20); // inclusive
+    std::normal_distribution<> normal_dist(0, 1); // mean = 1, std_dev = 1
+
+    /* step 3: get random numbers */
+    const int cnt = 5;
+    for (int i = 0; i < cnt; ++i)
+    {
+        std::cout << uniform_int_dist(rng) << " ";
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < cnt; ++i)
+    {
+        std::cout << uniform_real_dist(rng) << " ";
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < cnt; ++i)
+    {
+        std::cout << normal_dist(rng) << " ";
+    }
+
+    return 0;
+}
+
+/*
+   Q: Why does the modulo operation not preserve the uniformity of the uniform operand for rand()?
+   A: https://stackoverflow.com/questions/7114043/random-number-generation-in-c11-how-to-generate-how-does-it-work
+
+   Q: Why can we get any kind of desired distribution from uniform distribution?
+   A: https://en.wikipedia.org/wiki/Inverse_transform_sampling
+
+   Q: What is std::random_device?
+   A: std::random_device is supposed to be like an improved version of seeding with time(NULL).
+   Ref: https://stackoverflow.com/questions/39288595/why-not-just-use-random-device.
+
+   Q: What is std::std::mt19937?
+   A: std::mt19937  is a very simple and reliable random generator whose implementation derives from
+   the original mt19937 paper.
+   Ref: https://stackoverflow.com/questions/39288595/why-not-just-use-random-device
+
+   Q: Why to use mt19937 instead of rand()
+   A: rand() use old linear congruent LC algorithm. First, rand() need you to invoke srand(time(0))
+   on every run to ensure that it generates different sequence of numbers. Second, rand() produces
+   integers ranging from 0 to RAND_MAX by uniform distribution, inclusive. To specify the range,
+   you have to utilize modulo operation which break the uniformity. However, the mt19937 use std::
+   random_device to seed on every run for you and ensure the distribution type.
+*/
---
+++ random_number/demo_rand.cc
@@ -0,0 +1,50 @@
+// C++ program to demonstrate
+// the use of rand()
+// ref: https://www.geeksforgeeks.org/rand-and-srand-in-ccpp/
+// ref: https://stackoverflow.com/questions/52869166/why-is-the-use-of-rand-considered-bad
+#include <cstdlib> // for rand() and srand()
+#include <ctime>
+#include <iostream>
+
+int main()
+{
+	// This program will create same sequence of
+	// random numbers on every program run because the seed (starting point) is always the same
+    // as if srand(1) is called.
+	for (int i = 0; i < 5; i++)
+		std::cout << float(rand()) / float(RAND_MAX) << " ";
+    /* returns a pseudo-random number in the range of [0, RAND_MAX). */
+    /* RAND_MAX: is a constant whose default value may vary between implementations but it is
+       granted to be at least 32767. */
+    /*  It is uniform in this interval, which means that each number in this interval has the same probability to appear */
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << rand() % 10 << " ";
+    }
+
+    std::cout << std::endl;
+    /* Standard practice is to use the result of a call to srand(time(0)) as the seed.
+       However, time() returns a time_t value which varies every time and hence the pseudo-random
+       number varies for every program call. */
+     // Use current time as seed for random generator
+    /* time_t curr_time; */
+    srand(time(nullptr));
+    /* To get the uniform integer in the range of 0 to 10. */
+	for (int i = 0; i < 5; i++)
+		std::cout << rand() % 10 << " ";
+    std::cout << std::endl;
+
+    /* To get uniform variate in the range of 0.0 to 1.0. */
+	for (int i = 0; i < 5; i++)
+		std::cout << float(rand()) / float(RAND_MAX) << " ";
+
+	return 0;
+}
+
+/*
+Q: What kind of probability distribution is the rand() distributed as?
+A: Uniform distribution.
+*/
