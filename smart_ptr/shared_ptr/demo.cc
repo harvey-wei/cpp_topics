0d2bd96d03316932240161e07db59806a9703371
Harvey MAO <harvey.mao@outlook.com>
2022-10-16 13:47:26 +0800

    shared pointer

--- smart_ptr/shared_ptr/demo.cc
+++ smart_ptr/shared_ptr/demo.cc
@@ -1,47 +1,224 @@
-// C++ program to demonstrate shared_ptr
 #include <iostream>
-#include <memory>
+#include <string>
+#include <sstream>
 using namespace std;
 
-class A {
-public:
-	void show()
-	{
-		cout << "A::show()" << endl;
-	}
-    ~A(){ cout << "destroyed" << endl;}
+
+class Counter
+{
+    public:
+        // Constructor
+        Counter() : m_counter(0) {};
+
+        // Should not be assignable and copyable.
+        Counter& operator=(const Counter &other) = delete;
+        Counter(const Counter &other) = delete;
+
+        // Destructor
+        ~Counter() {};
+
+        void reset()
+        {
+            m_counter = 0;
+        };
+
+        unsigned int get()
+        {
+            return m_counter;
+        }
+
+        /* https://www.geeksforgeeks.org/increment-and-decrement-operator-overloading-in-c/ */
+        // Overload prefix increment operator
+        unsigned int operator++()
+        {
+            ++m_counter;
+
+            return m_counter;
+        }
+
+        // pass a dummy int to indicate postfix increment
+        unsigned int operator++(int)
+        {
+            unsigned int temp = m_counter;
+            m_counter++;
+
+            return temp;
+        }
+
+        // Overload prefix decrement operator
+        unsigned int operator--()
+        {
+            --m_counter;
+
+            return m_counter;
+        }
+
+        // Overload postfix decrement operator
+        unsigned int operator--(int)
+        {
+            unsigned int temp = m_counter;
+            m_counter--;
+
+            return temp;
+        }
+
+        // Overload stream operator
+        // https://www.geeksforgeeks.org/overloading-stream-insertion-operators-c/
+        // https://www.programiz.com/cpp-programming/friend-function-class
+        // When you make something a (non-static) member function, there is an implied first
+        // argument, the calling object
+        // The function will be automatically targeted into the surrounding namespace, i.e. global.
+        // object of ostream is on the left side of << while counter lies on the right side of <<.
+        // Ref: https://stackoverflow.com/questions/476272/how-to-properly-overload-the-operator-for-an-ostream
+        friend ostream& operator<<(ostream& os, const Counter &counter)
+        {
+            os << "Counter value: " << counter.m_counter << std::endl;
+
+            /* return ostream for chaining << */
+            return os;
+        }
+
+
+
+    private:
+        unsigned int m_counter;
 };
+
+template<class T>
+class Shared_Point
+{
+    public:
+        // Constructor receive raw pointer returned by new operator
+        // Note only be used one time.
+        explicit Shared_Point(T * ptr = nullptr)
+            :m_ptr(ptr), m_ref_cnt(new Counter())
+        {
+            if (nullptr != m_ptr) ++(*m_ref_cnt);
+        }
+
+        // Copy Constructor which can be used multiple times.
+        Shared_Point(const Shared_Point<T> &obj)
+        {
+            m_ptr = obj.m_ptr;
+            m_ref_cnt = obj.m_ref_cnt;
+            ++(*m_ref_cnt);
+        }
+
+        // Get the current reference count
+        unsigned int get_ref_cnt()
+        {
+            return m_ref_cnt->get();
+        }
+
+        // Get the pointer
+        T* get_ptr()
+        {
+            return m_ptr;
+        }
+
+        // Recall that smart pointer should also overload dereferencing and arrow operator.
+        // Overload * to return the reference to the object
+        T& operator*()
+        {
+            return *m_ptr;
+        }
+
+        // Overload -> to return the reference to const pointer to the object
+        T* const & operator->()
+        {
+            return m_ptr;
+        }
+
+        // Caveat: dot operator is unable to be overloaded.
+
+        // Destructor. Free up the resource pointed to by m_ptr if reference cnt reaches zero.
+        ~Shared_Point()
+        {
+            --(*m_ref_cnt);
+
+            if (0 == m_ref_cnt->get())
+            {
+                delete m_ptr;
+
+                // Don't forget to release the memory of the reference counter.
+                delete m_ref_cnt;
+                cout << "The raw pointer contained in shared pointer is deleted! " << endl;
+            }
+        }
+
+
+    private:
+        // m_ptr stores the raw pointer which is shared by multiple Shared_Point.
+        T * m_ptr;
+
+        // Record how many Shared_Point objects is the raw pointer shared by.
+        Counter * m_ref_cnt;
+
+    // Overload stream insertion operator << as a global function whose left hand operand
+    // is ostream &os and right hand operand is the second argument, namely ptr.
+    // Usage: cout << shared_ptr objcet
+    friend ostream & operator<<(ostream &os, Shared_Point<T> &ptr)
+    {
+        os << "Address pointed: " <<  ptr.get_ptr() << std::endl;
+        os << "Reference Count: " << ptr.get_ref_cnt << std::endl;
+    }
+};
+
+
+/* The first argument time is the left hand operand of + while the second argument is the right
+ * hand operand of +.
+ * Hence, string + int gives rise to error!
+ **/
+string operator+(int time, string suf)
+{
+    stringstream ss;
+    ss << time << suf;
+
+    return ss.str();
+}
 
 int main()
 {
-	shared_ptr<A> p1(new A);
-	cout << p1.get() << endl;
-	p1->show();
-	shared_ptr<A> p2(p1);
-	p2->show();
-	cout << p1.get() << endl;
-	cout << p2.get() << endl;
+    int hour = 9;
+    string suffix = "p.m.";
 
-	// Returns the number of shared_ptr objects
-	// referring to the same managed object.
-	cout << p1.use_count() << endl;
-	cout << p2.use_count() << endl;
+    string time = hour + suffix;
+    /* string time_ = suffix + hour;  // Error!*/
 
-	// Relinquishes ownership of p1 on the object
-	// and pointer becomes NULL
-	p1.reset();
-	cout << p1.get() << endl;
-	cout << p2.use_count() << endl;
-	cout << p2.get() << endl;
+    cout << time << endl;
 
-	return 0;
+
+    // new return an universal pointer and then implicitly type-cast!
+    Shared_Point<int> ptr_one(new int(132));
+    *ptr_one = 100;
+
+    Shared_Point<int> ptr_two(ptr_one);
+    *ptr_two = 200;
+
+    cout << "ptr_two's object is " << *ptr_two << endl;
+
+
+    return 0;
 }
 
-/** Q: When to use shared_ptr?
-  * A: Use shared_ptr if you want to share ownership of a resource.
-  * Many shared_ptr can point to a single resource. shared_ptr maintains reference count
-  * for this propose. when all shared_ptr pointers pointing to resource goes out of scope the resource
-  * is destroyed.
-  * ref: https://docs.microsoft.com/en-us/cpp/cpp/how-to-create-and-use-shared-ptr-instances?view=msvc-170
-  * ref: https://www.geeksforgeeks.org/auto_ptr-unique_ptr-shared_ptr-weak_ptr-2/
+/**
+  * Ref: https://www.geeksforgeeks.org/how-to-implement-user-defined-shared-pointers-in-c/
+  *
+  * Q: What is raw pointer?
+  * A: returned by new operator.
+  *
+  * Q: Why to overload stream operator as global?
+  * A: In operator overloading, if an operator is overloaded as a member, then it must be a
+  *    member of the object on the left side of the operator. For example, consider the statement
+  *    “ob1 + ob2” (let ob1 and ob2 be objects of two different classes). To make this statement
+  *    compile, we must overload ‘+’ in a class of ‘ob1’ or make ‘+’ a global function.
+  * Ref: https://www.geeksforgeeks.org/overloading-stream-insertion-operators-c/
+  * Ref: https://www.freecodecamp.org/news/how-to-overload-operators-in-cplusplus/
+  *
+  * Q: Why to use friend << ?
+  * Ref: https://leimao.github.io/blog/CMake-Public-Private-Interface/
+  *
+  * Q: For overloaded binary operators, which argument is the left hand side and which is the right
+  * hand side operand?
+  * Ref: https://ee.usc.edu/~redekopp/cs104/slides/L08a_OperatorOverloading.pdf
   */
---
+++ smart_ptr/shared_ptr/demo_std.cc
@@ -0,0 +1,47 @@
+// C++ program to demonstrate shared_ptr
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class A {
+public:
+	void show()
+	{
+		cout << "A::show()" << endl;
+	}
+    ~A(){ cout << "destroyed" << endl;}
+};
+
+int main()
+{
+	shared_ptr<A> p1(new A);
+	cout << p1.get() << endl;
+	p1->show();
+	shared_ptr<A> p2(p1);
+	p2->show();
+	cout << p1.get() << endl;
+	cout << p2.get() << endl;
+
+	// Returns the number of shared_ptr objects
+	// referring to the same managed object.
+	cout << p1.use_count() << endl;
+	cout << p2.use_count() << endl;
+
+	// Relinquishes ownership of p1 on the object
+	// and pointer becomes NULL
+	p1.reset();
+	cout << p1.get() << endl;
+	cout << p2.use_count() << endl;
+	cout << p2.get() << endl;
+
+	return 0;
+}
+
+/** Q: When to use shared_ptr?
+  * A: Use shared_ptr if you want to share ownership of a resource.
+  * Many shared_ptr can point to a single resource. shared_ptr maintains reference count
+  * for this propose. when all shared_ptr pointers pointing to resource goes out of scope the resource
+  * is destroyed.
+  * ref: https://docs.microsoft.com/en-us/cpp/cpp/how-to-create-and-use-shared-ptr-instances?view=msvc-170
+  * ref: https://www.geeksforgeeks.org/auto_ptr-unique_ptr-shared_ptr-weak_ptr-2/
+  */
