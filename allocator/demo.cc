6f28b830efeaf33eadc909e4ac739b579bdb8fb0
Harvey MAO <harvey.mao@outlook.com>
2023-01-02 00:00:04 +0800

    update

--- allocator/demo.cc
+++ allocator/demo.cc
@@ -7,13 +7,28 @@
 #include <iostream>
 #include <memory> // for std::allocator class
 
+class A
+{
+    public:
+        A()
+        {
+            std::cout << "construct A!" << std::endl;
+        };
+
+        ~A()
+        {
+            std::cout << "destruct A!" << std::endl;
+        }
+};
+
+
 int main()
 {
 
 	// allocator for integer values
 	std::allocator<int> myAllocator;
 
-	// allocate space for five ints
+	// allocate space on heap for five ints without constructing objects.
 	int* arr = myAllocator.allocate(5);
 
 	// construct arr[0] and arr[3]
@@ -24,8 +39,18 @@ int main()
 	std::cout << arr[3] << std::endl;
 	std::cout << arr[0] << std::endl;
 
-	// deallocate space for five ints
+	// deallocate space on heap for five ints
 	myAllocator.deallocate(arr, 5);
+
+    /* A* a = (A*) ::operator new(sizeof(A)); */
+    /* a->(A::A)(); */
+    /* a->~A(); */
+    /**/
+    /* if (a != 0) */
+    /* { */
+    /*     a->~A(); */
+    /*     ::operator delete(a); */
+    /* } */
 
 	return 0;
 }
@@ -36,11 +61,21 @@ int main()
  * It is used when you want to separate allocation and construction in two steps or when you want
  * to separate destruction and deallocation in two steps.
  *
+ * Where is std::allocator() allocate memory?
+ * std::allocator allocates from the free store, which is often called the heap because that is
+ * the data structure that is typically used to implement it.
+ * Ref: https://stackoverflow.com/questions/44741914/does-the-std-allocator-dynamically-allocate-memory-on-the-heap-does-it-safely-d
+ *
  * Bear it in mind:
  * All of the STL Containers (vector, list, set, map, stack, queue, etc.) in c++ have a type
  * paramter Allocator which defaults to be std::allocator. The default allocator simpy uses
  * new and delete to obtain and release memory.
  *
+ * allocatation vs construction and deallocation vs destruction
+ * The purpose of the allocator is to allocate raw memory without construction of objects,
+ * as well as simply deallocate memory without the need to destroy them,
+ *
  * https://medium.com/@terselich/1-a-guide-to-implement-a-simple-c-stl-allocator-705ede6b60e4
  * https://docs.ros.org/en/foxy/Tutorials/Advanced/Allocator-Template-Tutorial.html
+ * https://www.codeproject.com/Articles/4795/C-Standard-Allocator-An-Introduction-and-Implement
  */
---
+++ exceptions_stack_unwinding/demo.cc
@@ -0,0 +1,89 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class MyException{};
+class Dummy
+{
+    public:
+    Dummy(string s) : MyName(s) { PrintMsg("Created Dummy:"); }
+    Dummy(const Dummy& other) : MyName(other.MyName){ PrintMsg("Copy created Dummy:"); }
+    ~Dummy(){ PrintMsg("Destroyed Dummy:"); }
+    void PrintMsg(string s) { cout << s  << MyName <<  endl; }
+    string MyName;
+    int level;
+};
+
+void C(Dummy d, int i)
+{
+    cout << "Entering FunctionC" << endl;
+    d.MyName = " C";
+    throw MyException();
+
+    cout << "Exiting FunctionC" << endl;
+}
+
+void B(Dummy d, int i)
+{
+    cout << "Entering FunctionB" << endl;
+    d.MyName = "B";
+    C(d, i + 1);
+    cout << "Exiting FunctionB" << endl;
+}
+
+void A(Dummy d, int i)
+{
+    cout << "Entering FunctionA" << endl;
+    d.MyName = " A" ;
+  //  Dummy* pd = new Dummy("new Dummy"); //Not exception safe!!!
+    B(d, i + 1);
+ //   delete pd;
+    cout << "Exiting FunctionA" << endl;
+}
+
+int main()
+{
+    cout << "Entering main" << endl;
+    try
+    {
+        Dummy d(" M");
+        A(d,1);
+    }
+    catch (MyException& e)
+    {
+        cout << "Caught an exception of type: " << typeid(e).name() << endl;
+    }
+
+    cout << "Exiting main." << endl;
+    char c;
+    cin >> c;
+}
+
+/* Output:
+    Entering main
+    Created Dummy: M
+    Copy created Dummy: M
+    Entering FunctionA
+    Copy created Dummy: A
+    Entering FunctionB
+    Copy created Dummy: B
+    Entering FunctionC
+    Destroyed Dummy: C
+    Destroyed Dummy: B
+    Destroyed Dummy: A
+    Destroyed Dummy: M
+    Caught an exception of type: class MyException
+    Exiting main.
+
+*/
+
+
+/**
+ * Q: What is exception-related stack unwinding?
+ * A: In the C++ exception mechanism, control moves from the throw statement to the first catch
+ *    statement that can handle the thrown type. When the catch statement is reached, all of the
+ *    * automatic variables that are in scope between the throw and catch statements *  are destroyed in
+ *    a process that is known as stack unwinding.
+ * Ref: https://learn.microsoft.com/en-us/cpp/cpp/exceptions-and-stack-unwinding-in-cpp?view=msvc-170
+ * Ref: https://www.geeksforgeeks.org/stack-unwinding-in-c/
+ */
