f28f5f318fe43a5ce43396080f70c42c4b9d6ed5
harvey <harveywindy@gmail.com>
2025-04-13 12:51:04 +0800

    Add move semantics

--- toy_example/main.cpp
+++ toy_example/main.cpp
@@ -1,17 +1,31 @@
-#include "iostream"
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	int size(0);
-	cout << "Please enter a integer" << endl;
-	cin >> size;
+	/* int size(0); */
+	/* cout << "Please enter a integer" << endl; */
+	/* cin >> size; */
+	const int size = 10;
 	int arr[size];
 	for(int i(0);i < size; i++)
 	{
 		arr[i] = i*i;
 		cout << arr[i] << endl;
 	}
+
+	int x = 10;
+	int y = 11;
+	int z = x * y;
+
+	std::vector<int> a;
+	a.push_back(1);
+	a.push_back(2);
+	std::cout << "a[0]: " << a[0] << std::endl;
+
+	x = x * 100;
+	y = y * 1234;
 
 	return 0;
 }
